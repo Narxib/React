7e537ae63fdb78e7ec0c26fa39971ab55b7b2827
Brian <brianzaragoza25@gmail.com>
2023-11-22 11:59:33 +0100

    twitter card component

--- hola mundo/src/App.jsx
+++ hola mundo/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import "./App.css"
+export function App(){
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <article className="tw-follow-card">
+      <header className="tw-follow-card-header">
+        <img className="tw-follow-card-img" src="https://unavatar.io/kikobeats?ttl=1h" alt="avatar"/>
+        <div className="tw-follow-card-div">
+          <strong>User</strong>
+          <span className="tw-follow-card-span">@USER</span>
+        </div>
+      </header>
+      <aside>
+        <button className="tw-follow-card-button">Seguir</button>
+      </aside>
+    </article>
   )
 }
-
-export default App
